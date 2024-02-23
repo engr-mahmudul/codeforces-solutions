bfe50be2e43d6d16b2c1d50a94e7a6331113a6ba
Mahmudul Hasan <dev.mahmudulhasan@gmail.com>
2024-02-23 22:08:43 +0200

    Soloved problem

--- 7A-Way_Too_Long_Words.py
+++ 7A-Way_Too_Long_Words.py
@@ -3,7 +3,7 @@ container = [None] * counter
 for i in range(counter):
     container[i] = input()
 for i in container:
-    if len(i)< 10:
+    if len(i)<= 10:
         print(i)
     else:
         print("{}{}{}".format(i[0],len(i)-2,i[-1]))
