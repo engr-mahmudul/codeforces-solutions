3b56a38c940e5b18ff2f70274133fbde9823b4fc
Mahmudul Hasan <dev.mahmudulhasan@gmail.com>
2024-03-03 20:08:33 +0200

    solved fast prime number

--- 1221-Fast_Prime_Number.py
+++ 1221-Fast_Prime_Number.py
@@ -1,20 +1,26 @@
-import math
-test_case = int(input())
-array_container = [None]*test_case
-for i in range(test_case):
-    array_container[i] = int(input())
-for number in array_container:
+
+def prime_check(number):
     flag = 0
     if number == 0 or number == 1:
-        flag = 0
+        flag = 1
     elif number == 2:
+        flag = 0
+    elif number > 2 and number % 2 ==0:
         flag = 1
     else:
         for j in range(3, int(number ** 0.5) + 1, 2):
-            if number!= j and number % j == 0:
+            if number != j and number % j == 0:
                 flag = 1
                 break
     if flag > 0:
         print("Not Prime")
     else:
         print("Prime")
+
+test_case = int(input())
+array_container = [None]*test_case
+for i in range(test_case):
+    array_container[i] = int(input())
+for number in array_container:
+    prime_check(number)
+
